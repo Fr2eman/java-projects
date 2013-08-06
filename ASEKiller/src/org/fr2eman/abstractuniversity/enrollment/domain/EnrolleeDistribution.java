d20ee1f5cf62c4f650c2e48a990ec494b12d625e
Fr2eman <mishabobrikowich@gmail.com>
2013-08-06 19:36:52 +0300

    Added sort of enrollees

--- ASEKiller/src/org/fr2eman/abstractuniversity/enrollment/domain/EnrolleeDistribution.java
+++ ASEKiller/src/org/fr2eman/abstractuniversity/enrollment/domain/EnrolleeDistribution.java
@@ -11,15 +11,30 @@ public class EnrolleeDistribution {
 	private List<StudentList> listDistributedEnrollee;
 	
 	public List<StudentList> distribution(EnrolleeSource source) {
-		List<Enrollee> listEnrollee = new ArrayList<Enrollee>();
+		List<Enrollee> list = new ArrayList<Enrollee>();
 		try {
 			for (int i = 1; i <= 1000; i++) {
-				listEnrollee.add(source.nextEnrollee());
+				list.add(source.nextEnrollee());
 			}
 		} catch(NoMoreEnrolleeException e) {
 			e.printStackTrace();
 		}
-		
+		List<Enrollee> listEnrollee = new ArrayList<Enrollee>();
+		int maxScore = 0;
+		int index = 0;
+		int itorator = 0;
+		while(list.size() != 0) {
+			for(int i = 0; i < list.size(); i++) {
+				if(maxScore < list.get(i).getScore()) {
+					maxScore = list.get(i).getScore();
+					index = i;
+				}
+			}
+			listEnrollee.add(list.get(index));
+			list.remove(index);
+			itorator++;
+			maxScore = 0;
+		} 
 		return listDistributedEnrollee;
 	}
 	
